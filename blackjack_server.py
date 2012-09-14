e31545cde19aa9bb29b390822f0bfb9cecd791fe
tijko <oberon@oberon-Satellite-M645.(none)>
2012-09-14 03:53:11 -0400

    server for multijack

---
+++ blackjack_server.py
@@ -0,0 +1,31 @@
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+import simplejson
+
+class Chat(LineReceiver):
+    def __init__(self, users, players):
+        self.users = users
+        self.name = None
+        self.players = players
+
+    def connectionMade(self):
+        new = 'player_' + str(len(self.players) + 1)
+        self.players.append(new)
+        self.jason = self.players
+        self.jason = simplejson.dumps(self.jason) 
+#        self.sendLine(str(self.players,))       
+        self.sendLine(self.jason)
+
+class ChatFactory(Factory):
+    def __init__(self):
+        self.users = {} #maps instances to clients 
+        self.players = []
+
+    def buildProtocol(self, addr):
+        return Chat(self.users,self.players)
+
+
+reactor.listenTCP(6000, ChatFactory())
+reactor.run()
+
