b3f9be6fae5de05d041aac38cfed3433e971fc83
tijko <oberon@oberon-Satellite-M645.(none)>
2012-09-20 01:30:40 -0400

    players see the same hands for each other

--- multijack/second.py
+++ multijack/second.py
@@ -78,7 +78,43 @@ class Client(object):
 
     def new_line(self, line):
         self.line = line
-        self.line = simplejson.loads(self.line)
+        spot_x = 50
+        spot_y = 240
+        seat = 0
+        dspot_x = 260
+        if 'turn1' in self.line:
+            self.line = ''
+        if 'player' in line:
+            pass
+        if 'edge' in line:
+            self.line = simplejson.loads(self.line)
+            for i in self.line:
+                if 'png' not in i:
+                    edge = pygame.image.load('Pictures/cards/edge.png').convert()
+                    self.screen.blit(edge,(332,100))
+                if 'png' in i:
+                    out = pygame.image.load(('Pictures/cards/') + i)
+                    self.screen.blit(out,(dspot_x,100))
+                    dspot_x += 30
+        if 'png' in self.line and 'edge' not in self.line:
+            self.line = simplejson.loads(self.line)
+            for i in self.line:
+                for v in i:
+                    out = pygame.image.load(('Pictures/cards/') + v).convert()
+                    self.screen.blit(out,(spot_x,spot_y))
+                    spot_x += 30
+                if seat < 1:
+                    spot_x += 180
+                    spot_y += 120
+                if seat == 1:
+                    spot_x += 230
+                    spot_y -= 80
+                seat += 1
+        print self.line
+        pygame.display.flip()
+    
+    def sendLine(self):
+        pass
 
     def tick(self):
         flag = 0
@@ -187,42 +223,21 @@ class BlackClientProtocol(LineReceiver):
         self.recv = recv
 
     def lineReceived(self, line):
-        client = Client()
-        spot_x = 50
-        spot_y = 240
-        seat = 0
         self.recv(line)
-        if 'turn1' in line:
-            line = ''
-        if 'player' in line:
-            pass
-        if 'png' in line:
-            line = simplejson.loads(line)
-            for i in line:
-                for v in i:
-                    out = pygame.image.load(('Pictures/cards/') + v).convert()
-                    client.screen.blit(out,(spot_x,spot_y))
-                    spot_x += 30
-                if seat < 1:
-                    spot_x += 180
-                    spot_y += 120
-                if seat == 1:
-                    spot_x += 230
-                    spot_y -= 80
-                seat += 1
         print line
-        pygame.display.flip()
 
 class BlackClient(ClientFactory):
-    def __init__(self, recv):
-        self.recv = recv
+    def __init__(self, client):
+        self.client = client
 
     def buildProtocol(self, addr):
-        return BlackClientProtocol(self.recv)
+        proto = BlackClientProtocol(self.client.new_line)
+        self.client.sendLine = proto.sendLine
+        return proto
 
 if __name__ == '__main__':
     c = Client()
     lc = LoopingCall(c.tick)
     lc.start(0.1)
-    reactor.connectTCP('192.168.1.2', 6000, BlackClient(c.new_line))
+    reactor.connectTCP('192.168.1.2', 6000, BlackClient(c))
     reactor.run()
